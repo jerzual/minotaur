d6cc7372e126b90844b12c0e66b16272b92650ae
Benjamin Legrand <jibhaine@gmail.com>
2015-06-04 17:52:42 +0200

    added browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,36 @@
+'use strict';
+
+var browserify = require('browserify');
+var browserifyShim = require('browserify-shim');
+var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var gutil = require('gulp-util');
+var rename = require('gulp-rename');
+var less = require('gulp-less');
+var jade = require('gulp-jade');
+
+gulp.task('javascript', function () {
+  // set up the browserify instance on a task basis
+  var b = browserify({
+    entries: './src/scripts/main.js',
+    debug: true,
+    // defining transforms here will avoid crashing your stream
+    transform: [browserifyShim]
+  });
+
+  return b.bundle()
+    .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+        // Add transformation tasks to the pipeline here.
+        .pipe(uglify())
+        .on('error', gutil.log)
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist/js/'));
+});
 
 gulp.task('html', function () {
     // place code for your default task here
