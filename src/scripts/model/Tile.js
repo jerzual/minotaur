49ae4440448efd741d975d84896fb94367fb5987
Benjamin Legrand <benjilegnard@protonmail.com>
2016-01-25 21:36:17 +0100

    camel case in file names

---
+++ src/scripts/model/Tile.js
@@ -0,0 +1,9 @@
+/**
+ * a Tile is the atomic unit of the map. Every thing on the map should be on a tile.
+ */
+export default class Tile {
+    constructor() {
+        this.x = 0;
+        this.y = 0;
+    }
+}
