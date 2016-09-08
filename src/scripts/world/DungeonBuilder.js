6f83935e26b9bb6a5936d4f405877f24457a8b81
Benjamin Legrand <jibhaine@free.fr>
2016-09-08 17:37:40 +0200

    todo : fix tests

--- src/scripts/world/DungeonBuilder.js
+++ src/scripts/world/DungeonBuilder.js
@@ -1,5 +1,13 @@
+import LevelBuilder from './LevelBuilder';
+
+import Dungeon from '../model/Dungeon';
+import Seed from '../model/Seed';
+
 class DungeonBuilder {
-    
+    constructor({rng}) {
+        this.dungeon = new Dungeon();
+        this.rng = rng;
+    }
 }
 
 export default DungeonBuilder;
