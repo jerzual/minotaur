e13e8a892b7755fdeff8886a98a2ecb526df8401
Benjamin Legrand <jibhaine@free.fr>
2017-08-21 08:16:38 +0200

    refacto(tech-upgrade) : gulp to webpack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import Minotaur from './scripts/Minotaur';
+import style from './styles/index.scss';
 
+if(process.env.NODE_ENV === 'production') {
+ 	require('./scripts/utils/pwa');
+}
 document.addEventListener('DOMContentLoaded', (e) => {
     window.minotaur = new Minotaur(document.getElementById('minotaur'));
     window.minotaur.initialize(e);
