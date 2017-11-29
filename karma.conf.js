34c1292f98fd1bf3d53cabae3d7172fc0a19c886
Benjamin Legrand <jibhaine@gmail.com>
2017-11-29 07:59:35 +0100

    chore() upgrade & add deps

--- karma.conf.js
+++ karma.conf.js
@@ -4,7 +4,7 @@ module.exports = function(config)
 
         basePath: '',
 
-        frameworks: ['browserify', 'mocha','chai'],
+        frameworks: ['webpack', 'mocha','chai'],
 
         files: [
             'src/scripts/minotaur.js',
