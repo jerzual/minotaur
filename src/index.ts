eb89bd67b4961b52d65acf0d75097b7ee48d77ed
Benjamin Legrand <benjilegnard@protonmail.com>
2024-02-18 22:03:02 +0100

    fix: prod deployment

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 import Minotaur from './scripts/Minotaur';
 import style from './styles/index.scss';
 
-if (process.env.NODE_ENV === 'production') {
-	require('./scripts/utils/pwa');
-}
 document.addEventListener('DOMContentLoaded', (e) => {
 	const minotaur = new Minotaur(
 		document.getElementById('minotaur') as HTMLCanvasElement,
